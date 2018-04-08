9824f4c713c1d0d25324ea322b7f47ef1ec3d873
fuyanjun <fuyanjun@sendcloud.im>
2018-04-09 00:14:27 +0800

    base

---
+++ src/main/java/learn/sort/java/Base.java
@@ -0,0 +1,19 @@
+package learn.sort.java;
+
+public class Base {
+
+    public static void main(String[] args) {
+        int a = 1000;
+        System.out.println(a == new Integer(1000));
+        System.out.println(new Integer(1000).equals(1000));
+        System.out.println(new Integer(1000).equals(new Integer(1000)));
+        System.out.println(new Integer(10) == new Integer(10));
+        System.out.println(new Boolean(true) == new Boolean(true));
+        System.out.println(new String("abc") == new String("abc"));
+
+        Integer n = Integer.MAX_VALUE + 1;
+        System.out.println(n);
+        System.out.println(Integer.MAX_VALUE);
+    }
+
+}
