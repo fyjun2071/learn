d28cd8edd78b43aeec22c5f8e6bf9dd8f223d6ca
fuyanjun <fuyanjun@sendcloud.im>
2019-05-19 17:36:38 +0800

    动态规划

---
+++ python/sort/dynamic_programming.py
@@ -0,0 +1,26 @@
+# 动态规划
+
+arr = []
+
+
+# 递归方法
+def rec_opt(arr, i):
+    if i == 0:
+        return arr[0]
+    elif i == 1:
+        return max(arr[0], arr[1])
+    else:
+        A = opt[i - 2] + opt[i]
+        B = opt[i - 1]
+        return max(A, B)
+
+# 动态规划方法
+def dp_opt(arr):
+    opt = []
+    opt[0] = arr[0]
+    opt[1] = max(arr[0], arr[1])
+    for i in range(2, len(arr)):
+        A = opt[i - 2] + opt[i]
+        B = opt[i - 1]
+        opt[i] = max(A, B);
+    return opt[len(arr) - 1]
